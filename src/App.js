926d65c7b2f4d73fd8c035948f28e84686882a05
Jon Pinson <Jonathon.Pinson11@gmail.com>
2023-02-14 11:09:38 -0500

    Working Slides

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import SquaresBG from 'react-animated-squares';
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import SwiperCore, {Autoplay} from 'swiper';
+import 'swiper/css';
 
+SwiperCore.use([Autoplay])
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <div className='top'>
         <SquaresBG count={20} speed={0.9} backgroundColor="#3333333" />
         <div className='namePlate'>
@@ -16,20 +20,68 @@ function App() {
       </div>
 
       <div className='experience'>
-        <div className='slideshow'>
-          <div className='slide'>
+        <Swiper
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            DisableOnInteraction: false
+          }}
+        >
+          <SwiperSlide>
+            <div className='slideRow'>
+             <div className='slide'>
+              <img src='slide1.jpg' alt='related visual' />
+              </div>
+              <div className='textbox'>
+                <h2>Programming Languages</h2>
+                <p>A paragraph talking about it</p>
+              </div>          
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide2.jpg' alt='related visual' />
+                <div className='textbox'>
+                <h2>React and React Native</h2>
+                <p>A paragraph talking about it</p>
+              </div>          
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide2.jpg' alt='related visual' />
+                <div className='textbox'>
+                <h2>Database Training</h2>
+                <p>A paragraph talking about it</p>
+              </div>          
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide2.jpg' alt='related visual' />
+                <div className='textbox'>
+                <h2>Skill 4</h2>
+                <p>A paragraph talking about it</p>
+              </div>          
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
          
             
-          </div>
+      </div>
           <div className='textbox'>
           <h2>Experience</h2>
           <p>A paragraph or two talking about it.</p>
         </div>
        </div>
 
-      </div>
-       
-    </>
+     
   );
 }
 
